57d15612b4fc744701e1fd632ee3046a5dc8e4f9
Mur <uglanovmurad1999@gmail.com>
2024-05-23 12:03:16 +0300

    Обновить train, чтоб воспринимал русский язык

--- train.py
+++ train.py
@@ -2,11 +2,11 @@ import json
 import torch
 import numpy as np
 import torch.nn as nn
-from nltk_utils import tokenize, stem, bag_of_words
+from nltk_utils import tokenize_russian, stem, bag_of_words
 from torch.utils.data import Dataset, DataLoader
 from model import NeuralNet
 
-with open('intents.json', 'r') as f:
+with open('intents.json', 'r', encoding='utf-8') as f:
     intents = json.load(f)
 
 all_words = []
@@ -16,7 +16,7 @@ for intent in intents['intents']:
     tag = intent['tag']
     tags.append(tag)
     for pattern in intent['patterns']:
-        w = tokenize(pattern)
+        w = tokenize_russian(pattern)
         all_words.extend(w)
         xy.append((w, tag))
 
@@ -37,7 +37,6 @@ for (pattern_sentence, tag) in xy:
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 
-# Hyperparametrs
 batch_size = 8
 hidden_size = 8
 output_size = len(tags)
@@ -47,59 +46,49 @@ num_epochs = 1000
 
 class ChatDataset(Dataset):
     def __init__(self):
-        self.n_sampls = len(x_train)
+        self.n_samples = len(x_train)
         self.x_data = x_train
         self.y_data = y_train
 
-    #dataset[idx]
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
-    
-    def __len__(self):
-        return self.n_sampls
-    
 
+    def __len__(self):
+        return self.n_samples
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
-        
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
-# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-# Train the model
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
-        
-        # Forward pass
+
         outputs = model(words)
-        # if y would be one-hot, we must apply
-        # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
-        
-        # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
+
     if (epoch+1) % 100 == 0:
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 data = {
-"model_state": model.state_dict(),
-"input_size": input_size,
-"hidden_size": hidden_size,
-"output_size": output_size,
-"all_words": all_words,
-"tags": tags
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "hidden_size": hidden_size,
+    "output_size": output_size,
+    "all_words": all_words,
+    "tags": tags
 }
 
 FILE = "data.pth"
 torch.save(data, FILE)
 
-print(f'training complete. file saved to {FILE}')
+print(f'Обучение окончено. Файл сохранён как {FILE}')
