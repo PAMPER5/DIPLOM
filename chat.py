48915af7cb9faa0da6818862cff0525b1bd85a20
Mur <uglanovmurad1999@gmail.com>
2024-05-23 12:04:01 +0300

    Обновить chat, чтоб понримал и отвечал на русском

--- chat.py
+++ chat.py
@@ -2,11 +2,11 @@ import random
 import json
 import torch
 from model import NeuralNet
-from nltk_utils import bag_of_words, tokenize
+from nltk_utils import bag_of_words, tokenize_russian
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-with open('intents.json', 'r') as json_data:
+with open('intents.json', 'r', encoding='utf-8') as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
@@ -14,24 +14,24 @@ data = torch.load(FILE)
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
-output_size = data["output_size"]
+output_Multiplier = data["output_size"]
 all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model = NeuralNet(input_size, hidden_size, output_Multiplier).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Sam"
-print("Let's chat! (type 'quit' to exit)")
+bot_name = "Мой Бот"
+print("Давайте общаться! (напишите 'Выход' для окончания диалога)")
 while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
+    
+    sentence = input("Вы: ")
+    if sentence == "Выход":
         break
 
-    sentence = tokenize(sentence)
+    sentence = tokenize_russian(sentence)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
@@ -48,4 +48,4 @@ while True:
             if tag == intent["tag"]:
                 print(f"{bot_name}: {random.choice(intent['responses'])}")
     else:
-        print(f"{bot_name}: I do not understand...")
+        print(f"{bot_name}: Простите, я Вас не понял...")
