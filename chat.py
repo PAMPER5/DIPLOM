1f5aacc6dbed3b1eb273d036acaed9c69d79faf7
Mur <uglanovmurad1999@gmail.com>
2024-05-25 19:36:48 +0300

    обновить chat.py добавить функцию get_responce

--- chat.py
+++ chat.py
@@ -23,15 +23,11 @@ model = NeuralNet(input_size, hidden_size, output_Multiplier).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Мой Бот"
-print("Давайте общаться! (напишите 'Выход' для окончания диалога)")
-while True:
-    
-    sentence = input("Вы: ")
-    if sentence == "Выход":
-        break
+bot_name = "МГКИТ"
 
-    sentence = tokenize_russian(sentence)
+
+def get_response(msg):
+    sentence = tokenize_russian(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
@@ -46,6 +42,18 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: Простите, я Вас не понял...")
+                return random.choice(intent['responses'])
+    
+    return "Простите, я Вас не понял..."
+
+
+if __name__ == "__main__":
+    print("Давайте общаться! (напишите 'Выход' для окончания диалога)")
+    while True:
+    
+        sentence = input("Вы: ")
+        if sentence == "Выход":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
