b99e8dd707416d0d5d4c2c381691d2c2c3967614
Mur <uglanovmurad1999@gmail.com>
2024-06-02 16:29:57 +0300

    добавить маршрут для получения обновлённого json файла с десктопа

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from chat import get_response
+import json
 
 app = Flask(__name__)
 
@@ -8,10 +9,16 @@ def receive_message():
     text = request.get_json().get("message")
     response = get_response(text)
     
-    response = get_response(text)
-    
     # Возвращаем ответ в формате JSON
     return jsonify({'response': response})
 
+@app.route('/update_json', methods=['POST'])
+def update_json():
+    new_data = request.get_json()
+    with open('intents.json', 'w', encoding='utf-8') as json_file:
+        # Установка ensure_ascii=False для сохранения символов в их исходном виде
+        json.dump(new_data, json_file, ensure_ascii=False, indent=4)
+    return jsonify({'response': 'JSON успешно обновлён.'})
+
 if __name__ == "__main__":
-    app.run(debug=True, host='192.168.0.103', port=5000)
+    app.run(debug=True, host='192.168.0.104', port=5000)
