14e9f78326c49f38d98cb544c815bf111663b06f
Mur <uglanovmurad1999@gmail.com>
2024-06-05 22:44:47 +0300

    добавить маршрут для передачи json файла с вопросами

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from chat import get_response
 import json
 import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -21,6 +22,14 @@ def update_json():
     # Запускаем train.py асинхронно, чтобы не блокировать основной поток
     subprocess.Popen(['python', 'train.py'], shell=False)
     return jsonify({'response': 'JSON успешно обновлён и начат процесс обучения.'})
+
+@app.route('/get_questions', methods=['GET'])
+def get_questions():
+    # Убедитесь, что путь к файлу безопасен
+    if os.path.exists('questions.json'):
+        return send_from_directory(directory='.', path='questions.json', as_attachment=True)
+    else:
+        return jsonify({'response': 'Файл questions.json не найден.'})
 
 if __name__ == "__main__":
     app.run(debug=True, host='192.168.0.106', port=5000)
